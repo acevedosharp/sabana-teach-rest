a1bff30d19908c40d1e563bb148f63a67f03ff55
acevedosharp <acevedosharp@hotmail.com>
2021-04-19 14:12:25 -0500

    Disabled cors restrictions

--- src/main/kotlin/xyz/acevedosharp/sabanateachrest/SabanaTeachRestApplication.kt
+++ src/main/kotlin/xyz/acevedosharp/sabanateachrest/SabanaTeachRestApplication.kt
@@ -3,7 +3,12 @@ package xyz.acevedosharp.sabanateachrest
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
 
 @SpringBootApplication
 class SabanaTeachRestApplication {
@@ -12,4 +17,18 @@ class SabanaTeachRestApplication {
 
 fun main(args: Array<String>) {
     runApplication<SabanaTeachRestApplication>(*args)
+}
+
+
+@Configuration
+class MyConfiguration {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurerAdapter() {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+            }
+        }
+    }
 }
